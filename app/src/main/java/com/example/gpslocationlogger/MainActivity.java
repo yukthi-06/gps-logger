0c495f79ac9479f14e124620965fbfb2853ae654
Shibu TN <tnshibu@yahoo.com>
2026-05-11 14:37:48 +0530

    Status color codes

--- app/src/main/java/com/example/gpslocationlogger/MainActivity.java
+++ app/src/main/java/com/example/gpslocationlogger/MainActivity.java
@@ -24,6 +24,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
